e6063083237c2716fc6f2bb66ab2f7cc8f4f5dba
90dandan <1brokendrumhead@gmail.com>
2020-02-12 17:13:35 -0600

    light flashes

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-let simonPlayed, userPlayed, win, level, flash; 
+let simonPlayed, userPlayed, win, level, round; 
 
-document.querySelector("#c0").addEventListener('click', handleGreen)
-document.querySelector("#c1").addEventListener('click', handleBlue)
-document.querySelector("#c2").addEventListener('click', handleRed)
-document.querySelector("#c3").addEventListener('click', handleYellow)
+var c0 = document.querySelector("#c0").addEventListener('click', handleGreen)
+var c1 = document.querySelector("#c1").addEventListener('click', handleBlue)
+var c2 = document.querySelector("#c2").addEventListener('click', handleRed)
+var c3 = document.querySelector("#c3").addEventListener('click', handleYellow)
 document.getElementsByClassName("start")[0].addEventListener('click', start)
 
 function init() {
@@ -11,7 +11,7 @@ function init() {
     simonPlayed = [];
     userPlayed = [];
     level = 1;
-    flash = 1;
+    round = 1;
 }
 
 function start() {
@@ -19,18 +19,20 @@ function start() {
 }
 
 function handleGreen() {
-    userPlayed.push(1)
-    //flash function create an off on value in css for elements to change to for a period of time 
+    userPlayed.push(1) 
     console.log(userPlayed)
 }
+
 function handleBlue() {
     userPlayed.push(2)
     console.log(userPlayed)
 }
+
 function handleRed() {
     userPlayed.push(3)
     console.log(userPlayed)
 }
+
 function handleYellow() {
     userPlayed.push(4)
     console.log(userPlayed)
@@ -43,21 +45,39 @@ function createPattern() {
     }
 }
 
+function showPattern() {
+    for (var i = 0; i < round.length; i++) {
+        setTimeout(flash, 1000*i, simonPlayed[i]);
+    }
+}
 
-function userTurn() {
+function flash(drum) {
+ if (drum === 1){
+     c0.classList.c0.flash}
+ if (drum === 2){
+     c1.classList.c1.flash}
+ if (drum === 3){
+     c2.classList.c2.flash}
+ if (drum === 4){
+     c3.classList.c3.flash}            
+}
 
+function removeFlash() {
+    for (var i =0; i < round.length; i++) {
+        setTimeout(flash, 400, simonPlayed[i]);
+    }
 }
 
 function checkPattern() {
     if (userPlayed.length===simonPlayed.length){
-
+        
     }
 }
 
 init();
 
-//start button, starts showPattern
-//userPlayed push into array
+//start button, starts showPattern--
+//userPlayed push into array--
 //check length of array to match 
 //check for winning pattern
 //if winning, call showPattern
