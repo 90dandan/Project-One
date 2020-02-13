4dd2977ac952175a96b196d5bc63f19a7813e934
90dandan <1brokendrumhead@gmail.com>
2020-02-13 10:14:17 -0600

    handle colors

--- js/main.js
+++ js/main.js
@@ -20,22 +20,34 @@ function start() {
 
 function handleGreen() {
     userPlayed.push(1) 
+    c0.classList.replace('c0','c0.flash');
+    setTimeout()
     console.log(userPlayed)
 }
 
 function handleBlue() {
     userPlayed.push(2)
+    c1.classList.replace('c1','c1.flash');
+    setTimeout()
     console.log(userPlayed)
 }
 
 function handleRed() {
     userPlayed.push(3)
+    c2.classList.replace('c2','c2.flash');
+    setTimeout()
     console.log(userPlayed)
 }
 
 function handleYellow() {
     userPlayed.push(4)
+    c3.classList.replace('c3','c3.flash');
+    setTimeout()
     console.log(userPlayed)
+}
+
+function clickFlashOff() {
+    //if 
 }
 
 function createPattern() {
@@ -47,34 +59,40 @@ function createPattern() {
 
 function showPattern() {
     for (var i = 0; i < round.length; i++) {
-        setTimeout(flash, 1000*i, simonPlayed[i]);
+        setTimeout(flash(), 1000*i, simonPlayed[i]);
     }
 }
 
 function flash(drum) {
  if (drum === 1){
-     c0.classList.c0.flash}
+     c0.classList.replace('co0', 'c0.flash')}
  if (drum === 2){
-     c1.classList.c1.flash}
+     c1.classList.replace('c1', 'c1.flash')}
  if (drum === 3){
-     c2.classList.c2.flash}
+     c2.classList.replace('c2', 'c2.flash')}
  if (drum === 4){
-     c3.classList.c3.flash}            
+     c3.classList.replace('c3', 'c3.flash')}            
 }
 
 function removeFlash() {
     for (var i =0; i < round.length; i++) {
-        setTimeout(flash, 400, simonPlayed[i]);
+        setTimeout(flash(), 400, simonPlayed[i]);
     }
 }
 
 function checkPattern() {
-    if (userPlayed.length===simonPlayed.length){
-        
-    }
-}
+    if (userPlayed.length !== simonPlayed.length){
+        document.getElementById("message").innerHTML = "Bummer! Try Again!!"; 
+    } //else check through userPlayed and simonPlayed for match
+}   //if correct and level not 10 next level
+    //if level 10
+    // win = true
 
-init();
+function win() {
+   if (win = true) {
+       document.getElementById('message').innerHTML = "You Win!!"}
+
+    init();
 
 //start button, starts showPattern--
 //userPlayed push into array--
